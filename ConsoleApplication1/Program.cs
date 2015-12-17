ed8ddbfec4e1301deec92cc4b30ef2c4de36e993
Jovan Markovic <joca91markovic@hotmail.com>
2015-12-17 11:24:21 +0100

    3rd commit

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hallo World!!!");
+            System.Console.Read();
         }
     }
 }
